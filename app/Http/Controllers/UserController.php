22afc02f50fb4b3abe7cbc7e6253f5dfb28bf236
Ramón Franco Malta <ramonfrancomalta@gmail.com>
2021-05-20 09:42:27 -0500

    Hola que hace este es mi commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('users.userIndex',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        echo $user;
+        $request->validate([
+            'user_type' => ['string'],
+        ]);
+        User::where('id',$user->id)->update($request->only('user_type'));
+        return response()->json(['success'=>'Data is successfully updated']);
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ resources/views/users/userIndex.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.Arquitect') @section('Content')
+<div class="row">
+    <div class="col">
+      <div class="card">
+        <!-- Card header -->
+        <div class="card-header">
+            <div class="row align-items-center">
+              <div class="col-8">
+                <h3 class="mb-0">Usuarios</h3>
+              </div>
+              <div class="col-4 text-right">
+                <div class="form-group">
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">
+                          <input type="radio" aria-label="Radio button for following text input">
+                        </div>
+                      </div>
+                      <input type="text" class="form-control" aria-label="Text input with radio button">
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- Light table -->
+        <div class="table-responsive">
+          <table class="table align-items-center table-flush">
+            <thead class="thead-light">
+              <tr>
+                <th scope="col" class="sort" data-sort="name">Nombre</th>
+                <th scope="col" class="sort " data-sort="name">Email</th>
+                <th scope="col" class="sort " data-sort="name">Tipo de usuario</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody class="list">
+            @foreach($users as $user)
+              <tr>
+                <th scope="row navbar-nav align-items-center  ml-auto ml-md-0 ">
+                        <li class="nav-item dropdown">
+                          <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="media align-items-center">
+                              <span class="avatar avatar-sm rounded-circle">
+                                <img alt="Image placeholder" src="{{ $user->profile_photo_url}}">
+                              </span>
+                              <div class="media-body  ml-2  d-none d-lg-block">
+                                <span class="mb-0 text-sm  font-weight-bold">{{$user->name}}</span>
+                              </div>
+                            </div>
+                          </a>
+                        </li>
+                </th>
+                <td class="mail" >
+                  {{$user->email}}
+                </td>
+                <td>
+                        <form action="{{ route('User.update', [$user]) }}" method="POST" id="ajaxform" name="form">
+                            @method("PUT")
+                            {{ csrf_field() }}
+                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                            <select onchange="mySubmit(this.form)" name="userType" class="form-control" id="userType" value="value="{{old('userType')?? $user->user_type ?? ''}}">
+                                <option value="admin">Administrador</option>
+                                <option value="cliente">Usuario</option>
+                            </select>
+                        </form>
+
+                </td>
+
+                <td class="text-right">
+
+                        <form action="{{ route('User.destroy', [$user]) }}" method="POST" id="ajaxform" name="form">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-icon btn-outline-danger">
+                                <span class="btn-inner--icon">
+                                    <i class="fa fa-trash"></i>
+                                </span>
+                            </button>
+                        </form>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+        <!-- Card footer -->
+        <div class="card-footer py-4">
+          <nav aria-label="...">
+            <ul class="pagination justify-content-end mb-0">
+              <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">
+                  <i class="fas fa-angle-left"></i>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li>
+              <li class="page-item active">
+                <a class="page-link" href="#">1</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#">
+                  <i class="fas fa-angle-right"></i>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="http://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous">
+</script>
+<script>
+function mySubmit(theForm) {
+    console.log($(theForm).serialize());
+    $.ajax({ // create an AJAX call...
+        data: $(theForm).serialize(), // get the form data
+        type: $(theForm).attr('method'), // GET or POST
+        url: $(theForm).attr('action'), // the file to call
+        success: function (response) { // on success..
+            $('#here').html(response); // update the DIV
+            console.log(response);
+        },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
+    });
+}
+</script>
+@endsection
